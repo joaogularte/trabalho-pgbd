5573eb9da674d9774c1e2c314ac7ca084cbe0090
João Vitor Gularte <jov96@hotmail.com>
2019-06-11 18:51:34 -0300

    Adicionado outros metodos

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -2,22 +2,42 @@ const uuid = require('uuid/v4');
 const UserModel = require('../models/UserModel');
 
 class UserService {
-    
-    static async 
-    
+    static async list(){
+        return await UserModel.list();
+    }
+
+    static async get(id){
+        const user = await UserModel.get(id);
+        if(user.Item){
+            return user.Item;
+        }else{
+            return null;
+        }
+    }
+
     static async post(data){
         const id = uuid();
-
         const user = {
             userId: id,
             name: data.name,
             email: data.email
         }
-
         return await UserModel.post(user);
     }
 
+    
+    
+
 
 }
+
+const data = {
+    name: 'joaioja',
+    email: 'ijaiojdaiojda'
+}
+
+UserService.get('56dac56d-a4ef-4b1a-be2f-14d762be33a3')
+    .then(data => console.log(data))
+    .catch(e => console.log(e));
 
 module.exports = UserService;
