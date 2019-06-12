ca7a34e18d5a36be9e3e46ac8645fb441eb84a19
joaogularte <jov96@hotmail.com>
2019-06-12 00:05:04 -0300

    Vai

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -7,9 +7,50 @@ const params = {
 
 class UserModels{
 
+    static list(){
+        return dynamodb.scan(params).promise();
+    }
+
+    static get(id){
+        const key = { userId: id}
+        params.Key = key;
+        return dynamodb.get(params).promise();
+    }
+
     static post(data){
         params.Item = data;
         return dynamodb.put(params).promise();
+    }
+
+    static async put(id, data){
+        let updateExpression = "set ";
+        const expressionAttributes = {}
+        const key = { userId: id}
+
+        if(data.name){
+            console.log('entrou');
+            updateExpression += "nome = :nome, ";
+            expressionAttributes[":nome"] = data.name;
+        }   
+        if(data.email){
+            updateExpression +=  "email = :email";
+            expressionAttributes[":email"] = data.email;
+        } 
+        
+        params.Key = key;
+        params.ConditionExpression = 'attribute_exists(userId)'
+        params.UpdateExpression = updateExpression;
+        params.ExpressionAttributeValues = expressionAttributes;
+        params.ReturnValues = "UPDATED_NEW"
+
+        return dynamodb.update(params).promise();
+    }
+
+    static async delete(id){
+        const key = { userId: id }
+        params.Key = key;
+        params.ConditionExpression = 'attribute_exists(userId)'
+        return dynamodb.delete(params).promise();
     }
 }
 
